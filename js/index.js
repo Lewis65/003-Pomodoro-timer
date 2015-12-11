bccd946d44e5e2927bfedf69751b7291c0f2ca0a
Lewis Horwood <horwood.lewis@gmail.com>
2015-12-10 21:39:56 -0700

    Timer plays and pauses, and most of the guts are in place. Need to add functionality to adjust work and rest periods.

---
+++ js/index.js
@@ -0,0 +1,79 @@
+var active = false;
+var minutes = 0;
+var seconds = 0;
+var working = false;
+var intervalID = null;
+
+var switchTimer = function(){
+	//Do stuff like play a sound
+	if(working === true){
+		//switch to rest timer
+		minutes = document.getElementById("rest-setting").innerHTML;
+		seconds = 0;
+		working = false;
+	} else if (working === false){
+		//switch to work timer
+		minutes = document.getElementById("work-setting").innerHTML;
+		seconds = 0;
+		working = true;
+	}
+	updateClock();
+};
+
+//working
+var countdown = function(){
+	if(seconds > 0){
+		seconds -= 1;
+	} else if(seconds === 0 && minutes > 0){
+		seconds = 59;
+		minutes -= 1;
+	}
+	updateClock();
+};
+
+//working
+var timeString = function(x){
+  if(x < 10) {
+		return "0" + x;
+	} else {
+		return x;
+	}
+};
+
+//working
+var updateClock = function(){
+	var newMins = timeString(minutes);
+	var newSecs = timeString(seconds);
+	$("#clock-minutes").text(newMins);
+	$("#clock-seconds").text(newSecs);
+	if(newMins == 0 && newSecs == 0){
+		switchTimer();
+	}
+}
+
+//working
+var activeChange = function(){
+	if(active === true){
+		intervalID = setInterval(countdown, 1000);
+	} else {
+		clearInterval(intervalID);
+	}
+}
+
+$("#button-play").click(function(){
+	$("#button-pause").removeClass("nodisplay");
+	$("#button-play").addClass("nodisplay");
+	active = true;
+	activeChange();
+});
+
+$("#button-pause").click(function(){
+	$("#button-play").removeClass("nodisplay");
+	$("#button-pause").addClass("nodisplay");
+	active = false;
+	activeChange();
+});
+
+$(document).ready(function(){
+	switchTimer();
+});
